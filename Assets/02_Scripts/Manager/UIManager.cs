e5cb33daba43277f9f2c12a4c18365bd8a2e4dde
SJT <tlawlxo78@gmail.com>
2026-07-29 16:19:33 +0900

    puase 완성
    puase 완성

--- Assets/02_Scripts/Manager/UIManager.cs
+++ Assets/02_Scripts/Manager/UIManager.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] TMP_Text puaseText;
     [SerializeField] TMP_Text restartText;
     [SerializeField] TMP_Text surviveTimerText;
     float surviveTimer_Float;
@@ -11,6 +12,7 @@ public class UIManager : MonoBehaviour
     void Awake()
     {
         restartText.gameObject.SetActive(false);
+        puaseText.gameObject.SetActive(false);
     }
 
     void Start()
@@ -23,17 +25,34 @@ public class UIManager : MonoBehaviour
         if (GameManager.Instance.IsGameOver)
             return;
 
+        Puase();
         SetSurviveTimerText();
     }
 
     void SetSurviveTimerText()
     {
         surviveTimer_Float += Time.deltaTime;
-        Debug.Log(surviveTimer_Float);
         if (surviveTimer_Int != (int)surviveTimer_Float)
         {
             surviveTimer_Int = (int)surviveTimer_Float;
             surviveTimerText.text = $"survive time : {surviveTimer_Int}";
+        }
+    }
+
+    void Puase()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (puaseText.gameObject.activeSelf)
+            {
+                Time.timeScale = 1;
+                puaseText.gameObject.SetActive(false);
+            }
+            else
+            {
+                Time.timeScale = 0;
+                puaseText.gameObject.SetActive(true);
+            }
         }
     }
 }
